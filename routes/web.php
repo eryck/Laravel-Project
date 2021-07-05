19a335649f391690d3b556d730e905da15522a4d
Eryck Kaique <eryckknl@hotmail.com>
2021-07-04 23:08:35 -0300

    Know the Blade
    Know the Blade

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,8 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+
+    $nome = "Matheus";
+
+    return view('welcome', ['nome' => $nome]);
 });
