421732b082c08243ecd89c4232c90562b37fc219
Eryck Kaique <eryckknl@hotmail.com>
2021-07-04 23:37:14 -0300

    Advanced blade engine templet
    Advanced blade engine templet

--- routes/web.php
+++ routes/web.php
@@ -16,6 +16,18 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
 
     $nome = "Matheus";
+    $idade = 29;
+    $arr = [1, 2, 3, 4, 5];
+    $nomes = ["Matheus", "Maria", "Cristiano", "Saulo"];
 
-    return view('welcome', ['nome' => $nome]);
+    return view(
+        'welcome',
+        [
+            'nome' => $nome,
+            'idade' => $idade,
+            'profissao' => "Programador",
+            'arr' => $arr,
+            'nomes' => $nomes
+        ]
+    );
 });
