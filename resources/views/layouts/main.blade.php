278d9919870788e9681b2d1a340ffaf2760da7fe
Eryck Kaique <eryckknl@hotmail.com>
2021-07-07 00:06:59 -0300

    Rescuing url parameters
    Rescuing url parameters

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -11,20 +11,41 @@
     <link href="https://fonts.googleapis.com/css2?family=Roboto" rel="stylesheet">
 
     <!-- CSS BootStrap -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" 
-    rel="stylesheet" 
-    integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" 
-    crossorigin="anonymous">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
 
     <!-- CSS da aplicação -->
     <link rel="stylesheet" href="/css/style.css">
-   <script src="/js/scripts.js"></script>
+    <script src="/js/scripts.js"></script>
 </head>
-@yield('content')
 <body>
-<footer>
-    <p>HDC Events &copy; 2021</p>
-</footer>
+    <header>
+        <nav class="navbar navbar-expand-lg navbar-light">
+            <div class="collapse navbar-collapse" id="navbar">
+                <a href="/" class="navbar-brand">
+                    <img src="/img/hdcevents_logo.svg" alt="Logo HD Events">
+
+                </a>
+                <ul class="navbar-nav">
+                    <li class="nav-item">
+                        <a href="/" class="nav-link">Eventos</a>
+                    </li>
+                    <li class="nav-item">
+                        <a href="/" class="nav-link">Criar Eventos</a>
+                    </li>
+                    <li class="nav-item">
+                        <a href="/" class="nav-link">Entrar</a>
+                    </li>
+                    <li class="nav-item">
+                        <a href="/" class="nav-link">Cadastrar</a>
+                    </li>
+                </ul>
+            </div>
+        </nav>
+    </header>
+    @yield('content')
+    <footer>
+        <p>HDC Events &copy; 2021</p>
+    </footer>
 </body>
 
 </html>
